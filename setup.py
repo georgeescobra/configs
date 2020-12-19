b8fd4a0fcb00437a4f1b9227126c24b9831d9332
George <tgeorge.escobar@gmail.com>
2020-12-18 18:14:36 -0800

    asd

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
-#!/usr/bin/env python3
 import os
 import sys
+"""
+this assumes that the config files are located in home directory ~
 # need to be able to update the files
 # need to be able to setup the config
 # i am not sure whether or not i will need to know the opeprating system
 # this is going to be mostly bash (linux/unix)
+"""
+
 def update():
-    print('updating')
-    return 1
+    """ copies necessary files from ~/ to . """
+    os.system("cp ~/.vimrc ~/configs/vimrc.txt")
+    os.system("cp ~/.tmux.conf ~/configs/tmux.conf")
+    os.system("cp ~/.bashrc ~/configs/bashrc.txt")
+    orig = os.getcwd()
+    os.chdir(orig)
+    diff = os.popen("git diff").read()
+    print(diff)
+    #os.system("git add .")
+    #os.system("git commit -m 'another update'")
+    #os.system("git push")
+    #os.chdir(orig)
 
 def install():
-    print('install')
+    """ copies necessary files from . to ~/"""
+    os.system("cp ./vimrc.txt ~/.vimrc")
+    os.system("cp ./tmux.conf ~/.tmux.conf")
+    os.system("cp ./bashrc.txt ~/.bashrc")
+
 
 flags = {
     'update' : update,
@@ -27,8 +44,8 @@ def main():
     if hasFlag:
         flag = args.pop(0)
         flag = flag.strip('--')
-        if ret := flags.get(flag, None):
-            ret()
+        if func := flags.get(flag, None):
+            func()
         else: print('FLAG DOES NOT EXIST')
 
 if __name__ == "__main__": main()
