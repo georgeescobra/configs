36ee99eb802d04982b0d74e778df6eda67483d5c
George <tgeorge.escobar@gmail.com>
2020-12-18 20:21:19 -0800

    Updated:  .gitignore setup.py ubuntuDirColors.txt vimrc.txt dir.colors.txt tmux.conf.txt on 18/12/2020 20:21:19

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@ import os
 import sys
 """
 this assumes that the config files are located in home directory ~
-# need to be able to update the files
+needs python3.8 at least
 # need to be able to setup the config
 # i am not sure whether or not i will need to know the opeprating system
 # this is going to be mostly bash (linux/unix)
 """
+HOME = os.path.expanduser("~")
 
 def update():
-    """ copies necessary files from ~/ to . """
-    os.system("cp ~/.vimrc ~/configs/vimrc.txt")
-    os.system("cp ~/.tmux.conf ~/configs/tmux.conf")
-    os.system("cp ~/.bashrc ~/configs/bashrc.txt")
+    """ copies necessary files from ~/ to . 
+        Then pushes to github automatically
+    """
+    os.system(f"cp {HOME}/.vimrc {HOME}/configs/vimrc.txt")
+    os.system(f"cp {HOME}/.tmux.conf {HOME}/configs/tmux.conf")
+    os.system(f"cp {HOME}/.bashrc {HOME}/configs/bashrc.txt")
     orig = os.getcwd()
     os.chdir(orig)
     diff = os.popen("git diff").read()
@@ -32,7 +35,6 @@ def update():
                 line = line.split()
                 nameOfFiles.append(line[0])   
             if "modified:" in line or "deleted:" in line:
-                print(line)
                 line = line.split()
                 nameOfFiles.append(line[1])
                
@@ -45,21 +47,36 @@ def update():
         print("No differences since last update")
 
 def install():
-    """ copies necessary files from . to ~/"""
-    os.system("cp ./vimrc.txt ~/.vimrc")
-    os.system("cp ./tmux.conf ~/.tmux.conf")
-    os.system("cp ./bashrc.txt ~/.bashrc")
+    """ copies necessary files from . to ~/
+        If cache exists then install already happened
+    """
+    cacheDir = os.getcwd() + '/cache'
+    cwd = os.getcwd()
+    os.mkdir(cacheDir)
+    listOfFiles = os.listdir(cwd)
+    print(listOfFiles)
 
+    
+    if os.path.isdir(cacheDir) and not len(os.listdir(cacheDir)): 
+        print("NO BASE CONFIGS FOUND DELETING CACHE")
+        os.rmdir(cacheDir)
+
+def reset():
+    """ resets to original files from cache
+    """
+    cacheDir = os.getcwd() + '/cache'
+    os.system(f"cp ./vimrc.txt {HOME}/.vimrc")
+    os.system(f"cp ./tmux.conf {HOME}/.tmux.conf")
+    os.system(f"cp ./bashrc.txt {HOME}/.bashrc") 
 
 flags = {
     'update' : update,
     'install' : install,
-    'reset' : install
+    'reset' : reset
 }
 def main():
     args = sys.argv
     args.pop(0)
-    print(args)
     hasFlag = False
     if args: hasFlag = True if '--' in args[0] else False
     else: install() # this will be default if only setup.py is ran
